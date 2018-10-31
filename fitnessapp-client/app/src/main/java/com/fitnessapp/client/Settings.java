8600f22b4cdf9f75a09ad6f857e626b54b9f5e2e
Alexandru Martinas <alexandrumartinas33@gmail.com>
2018-11-01 00:16:50 +0100

    Added layouts

---
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/Settings.java
@@ -0,0 +1,69 @@
+package com.fitnessapp.client;
+
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+
+public class Settings extends Activity implements AdapterView.OnItemSelectedListener {
+
+    TextView mTv;
+    Button mBtn;
+    Calendar c;
+    DatePickerDialog dpd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        mTv = (TextView) findViewById(R.id.textView4);
+        mBtn = (Button) findViewById(R.id.buttonPick);
+
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(Settings.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int mYear, int mMonth, int mDay) {
+                        mTv.setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
+                    }
+                }, day, month, year);
+                dpd.show();
+            }
+        });
+
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
