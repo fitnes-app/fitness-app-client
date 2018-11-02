1a5c8f6d9f95fe3e8b1c18dbbab687ea959e62d8
Erox7 <trugove@hotmail.com>
2018-11-02 12:45:55 +0100

    mainpage started

---
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/Utils/Table.java
@@ -0,0 +1,105 @@
+package com.fitnessapp.client.Utils;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.fitnessapp.client.R;
+
+import java.util.ArrayList;
+
+public class Table {
+    private TableLayout table; // Layout donde se pintará la tabla
+    private ArrayList<TableRow> row; // Array de las filas de la tabla
+    private Activity actividad;
+    private Resources rs;
+    private int ROWS, COLUMNS; // Filas y columnas de nuestra tabla
+
+    public Table(Activity actividad, TableLayout table)
+    {
+        this.actividad = actividad;
+        this.table = table;
+        rs = this.actividad.getResources();
+        ROWS = COLUMNS = 0;
+        row = new ArrayList<>();
+    }
+
+    /**
+     * Añade la cabecera a la tabla
+     * @param recursocabecera Recurso (array) donde se encuentra la cabecera de la tabla
+     */
+    public void agregarCabecera(int recursocabecera)
+    {
+        TableRow.LayoutParams layoutCelda;
+        TableRow fila = new TableRow(actividad);
+        TableRow.LayoutParams layoutFila = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        fila.setLayoutParams(layoutFila);
+
+        String[] arraycabecera = rs.getStringArray(recursocabecera);
+        COLUMNS = arraycabecera.length;
+
+        for(int i = 0; i < arraycabecera.length; i++)
+        {
+            TextView texto = new TextView(actividad);
+            layoutCelda = new TableRow.LayoutParams(obtenerAnchoPixelesTexto(arraycabecera[i]), TableRow.LayoutParams.WRAP_CONTENT);
+            texto.setText(arraycabecera[i]);
+            texto.setGravity(Gravity.CENTER_HORIZONTAL);
+            texto.setLayoutParams(layoutCelda);
+
+            fila.addView(texto);
+        }
+
+        table.addView(fila);
+        row.add(fila);
+
+        ROWS++;
+    }
+
+    /**
+     * Agrega una fila a la tabla
+     * @param elementos Elementos de la fila
+     */
+    public void agregarFilaTabla(ArrayList<String> elementos)
+    {
+        TableRow.LayoutParams layoutCelda;
+        TableRow.LayoutParams layoutFila = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        TableRow fila = new TableRow(actividad);
+        fila.setLayoutParams(layoutFila);
+
+        for(int i = 0; i< elementos.size(); i++)
+        {
+            TextView texto = new TextView(actividad);
+            texto.setText(String.valueOf(elementos.get(i)));
+            texto.setGravity(Gravity.CENTER_HORIZONTAL);
+            layoutCelda = new TableRow.LayoutParams(obtenerAnchoPixelesTexto(texto.getText().toString()), TableRow.LayoutParams.WRAP_CONTENT);
+            texto.setLayoutParams(layoutCelda);
+
+            fila.addView(texto);
+        }
+
+        table.addView(fila);
+        row.add(fila);
+
+        ROWS++;
+    }
+
+    /**
+     * Obtiene el ancho en píxeles de un texto en un String
+     * @param texto Texto
+     * @return Ancho en píxeles del texto
+     */
+    private int obtenerAnchoPixelesTexto(String texto)
+    {
+        Paint p = new Paint();
+        Rect bounds = new Rect();
+        p.setTextSize(50);
+
+        p.getTextBounds(texto, 0, texto.length(), bounds);
+        return bounds.width();
+    }
+}
