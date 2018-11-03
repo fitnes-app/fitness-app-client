d9346603c8b069a99171e39efbf3e4358365bf84
Jordi Godia <jgm17@alumnes.udl.cat>
2018-11-03 19:26:33 +0100

    updated AndroidManifest & values files for Register

--- fitnessapp-client/app/src/main/java/com/fitnessapp/client/RegisterActivity.java
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/RegisterActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+
 public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +14,23 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.register);
 
         Spinner spinnerRoles = (Spinner) findViewById(R.id.spinnerRoles);
-        String[] roles = {"Trainer","Simple User"};
-        spinnerRoles.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, roles));
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterRoles = ArrayAdapter.createFromResource(this, R.array.rolesOptions, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapterRoles.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        // Apply the adapter to the spinner
+        spinnerRoles.setAdapter(adapterRoles);
 
         Spinner spinnerSpecialities = (Spinner) findViewById(R.id.spinnerSpecialities);
-        String[] specialities = {"Swimming","Crossfit", "Athletism", "Climbing"};
-        spinnerSpecialities.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, specialities));
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterSpecialities = ArrayAdapter.createFromResource(this, R.array.specialitiesOptions, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapterSpecialities.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        // Apply the adapter to the spinner
+        spinnerSpecialities.setAdapter(adapterSpecialities);
+
     }
 
 
