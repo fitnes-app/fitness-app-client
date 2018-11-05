ddc7bd51c08bf2c55e953fd1d18ccffc816be7d6
Erox7 <trugove@hotmail.com>
2018-11-05 12:56:52 +0100

    Activity to put Menu in Trainer flow created

---
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/BaseDrawerActivityTrainer.java
@@ -0,0 +1,12 @@
+package com.fitnessapp.client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseDrawerActivityTrainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
