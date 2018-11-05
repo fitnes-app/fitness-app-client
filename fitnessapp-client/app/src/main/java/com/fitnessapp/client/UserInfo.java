ab91fce9b4f53d5e4115e80515f64c5c944d92dd
Alexandru Martinas <alexandrumartinas33@gmail.com>
2018-11-05 18:05:57 +0100

    Added back buttons on some layouts

--- fitnessapp-client/app/src/main/java/com/fitnessapp/client/UserInfo.java
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/UserInfo.java
@@ -1,7 +1,10 @@
 package com.fitnessapp.client;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserInfo extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class UserInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        Button buttonBack = (Button) findViewById(R.id.buttonBack);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                back(v);
+            }
+        });
+
+    }
+
+    public void back(View view) {
+        // Prepare el moviment dsde la clase que estas fins a DisplayMessage...
+        Intent intent = new Intent(this, MainPageActivity.class);
+        //Efectua el cambi de activity
+        startActivity(intent);
     }
 }
