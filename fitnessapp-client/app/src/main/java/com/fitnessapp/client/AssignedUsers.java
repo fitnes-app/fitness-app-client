d925f1bcf35d9fb222a46a60143fb8bf224d321d
Erox7 <trugove@hotmail.com>
2018-11-05 13:38:41 +0100

    trainer menu done, AssignedUsers and TrainerMainPage completed

--- fitnessapp-client/app/src/main/java/com/fitnessapp/client/AssignedUsers.java
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/AssignedUsers.java
@@ -3,11 +3,11 @@ package com.fitnessapp.client;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class AssignedUsers extends AppCompatActivity {
+public class AssignedUsers extends BaseDrawerActivityTrainer {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.assigned_users);
+        getLayoutInflater().inflate(R.layout.assigned_users, frameLayout);
     }
 }
