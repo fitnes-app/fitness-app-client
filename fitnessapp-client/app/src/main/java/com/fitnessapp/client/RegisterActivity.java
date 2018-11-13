f34539dd19f06c76a7f297e676a5445dd74ded2b
Erox7 <trugove@hotmail.com>
2018-11-13 16:44:31 +0100

    Class User created

--- fitnessapp-client/app/src/main/java/com/fitnessapp/client/RegisterActivity.java
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/RegisterActivity.java
@@ -17,16 +17,23 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private EditText username,password,email;
+    private FirebaseDatabase db;
+    private DatabaseReference myRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        myRef = db.getReference();
         username = findViewById(R.id.editTextUser);
         password = findViewById(R.id.editTextPwd);
         email = findViewById(R.id.editTextEmail);
---
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/utils/User.java
@@ -0,0 +1,54 @@
+package com.fitnessapp.client.utils;
+
+public class User {
+
+    private String name, email, password, role, speciality;
+
+    public User(String name,String email,String password,String role,String speciality){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.speciality = speciality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
