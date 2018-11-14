c423c9c78a38f93cc123430ac8c5fe978f9eca67
Erox7 <trugove@hotmail.com>
2018-11-14 16:53:36 +0100

    Login functionality done, test time

--- fitnessapp-client/app/src/main/java/com/fitnessapp/client/LoginActivity.java
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/LoginActivity.java
@@ -1,75 +1,94 @@
 package com.fitnessapp.client;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    private Button registerButton, loginButton;
+    private EditText emailET, passwdET;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout2);
-        Spinner spinner = findViewById(R.id.spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.loginOptions, R.layout.spinnerlayout);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
 
-        spinner.setOnItemSelectedListener(
-                new AdapterView.OnItemSelectedListener() {
-                    public void onItemSelected(AdapterView<?> spn,
-                                               android.view.View v,
-                                               int posicion,
-                                               long id) {
+        emailET = findViewById(R.id.loginEmailET);
+        passwdET = findViewById(R.id.loginPasswrdET);
+
+        registerButton = findViewById(R.id.registerButton);
+        loginButton = findViewById(R.id.loginButton);
+
+        registerButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
 
 
-                        if(spn.getItemAtPosition(posicion).toString().equals("User Login")){
-                            goUserMainPage(v);
-                        }
-                        if(spn.getItemAtPosition(posicion).toString().equals("Trainer Login")){
-                            goTrainerMainPage(v);
-                        }
-
-                    }
-                    public void onNothingSelected(AdapterView<?> spn) {
-                    }
-                });
-        Button button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                register(v);
-            }
-        });
     }
 
-    public void goUserMainPage(View view) {
+    @Override
+    public void onClick(View view) {
+        String email = emailET.getText().toString();
+        String password = passwdET.getText().toString();
+        if(!email.equals("") && password.equals("")){
+            switch(view.getId()) {
+                case R.id.loginButton:
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        System.out.println(myRef.child("Users").child(user.getUid()).getKey());
+
+                                    } else {
+
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                case R.id.registerButton:
+                    register();
+            }
+        }
+    }
+
+    public void goUserMainPage() {
 
         Intent intent = new Intent(this, MainPageActivity.class);
         startActivity(intent);
     }
 
-    public void goTrainerMainPage(View view) {
+    public void goTrainerMainPage() {
 
         Intent intent = new Intent(this, TrainerMainPage.class);
-        //Efectua el cambi de activity
         startActivity(intent);
     }
 
-    public void register(View view) {
+    public void register() {
 
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
