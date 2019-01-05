002a789d5fd7f5b4600ae7ce0885d09aca3b049b
Erox7 <trugove@hotmail.com>
2019-01-05 01:46:41 +0100

    Progress tracker now receives daily exercices information

--- fitnessapp-client/app/src/main/java/com/fitnessapp/client/Fragments/ProgressTrackerFragment.java
+++ fitnessapp-client/app/src/main/java/com/fitnessapp/client/Fragments/ProgressTrackerFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -30,14 +31,20 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class ProgressTrackerFragment extends Fragment {
+public class ProgressTrackerFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private LineChart mChart;
+    private Boolean isPremium;
+    private String workoutId = "", workoutDuration = "";
+    private ArrayList<String> exercicesNames = new ArrayList<>();
     private UrlConnectorGetProgress ucgp;
     private URL url;
     private HttpURLConnection conn;
     private BaseDrawerActivity activity;
+    private View rootView;
     public ProgressTrackerFragment(){}
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,10 +55,10 @@ public class ProgressTrackerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View RootView = inflater.inflate(R.layout.fragment_progress_tracker, container, false);
+        rootView = inflater.inflate(R.layout.fragment_progress_tracker, container, false);
         activity = (BaseDrawerActivity)getActivity();
-        loadData(RootView);
-        return RootView;
+        loadData(rootView);
+        return rootView;
     }
 
 
@@ -115,6 +122,23 @@ public class ProgressTrackerFragment extends Fragment {
 
         return yVals;
     }
+
+    public void setSpinnerData(View rootView){
+        Spinner exercices = rootView.findViewById(R.id.spinnerRoutines);
+        exercices.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, this.exercicesNames));
+        exercices.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
     private class UrlConnectorGetProgress extends AsyncTask<Void,Void,Void> {
 
 
@@ -122,55 +146,46 @@ public class ProgressTrackerFragment extends Fragment {
         protected Void doInBackground(Void... params) {
             try {
 
-                URL url = new URL(StaticStrings.ipserver  + "/client/find/" + activity.userId);
+                URL url = new URL(StaticStrings.ipserver + "/client/" + activity.userId);
                 System.out.println(url);
                 conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("GET");
                 conn.setRequestProperty("Accept", "application/json");
+                JSONObject workout, user;
                 int userID = -1;
 
-                if(conn.getResponseCode() == 200){
+                if (conn.getResponseCode() == 200) {
                     BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                     String output = br.readLine();
                     JSONArray arr = new JSONArray(output);
-                    JSONObject user = arr.getJSONObject(0);
+                    user = arr.getJSONObject(0);
                     userID = user.getInt("id");
+                    isPremium = user.getBoolean("is_Premium");
                     System.out.println("USER: " + user);
+
+                    if (user.has("basicWorkout")) {
+                        workout = user.getJSONObject("basicWorkout");
+                        workoutDuration = workout.getString("duration");
+                        workoutId = workout.getString("id");
+                        setBasicWorkout();
+                    } else if (user.has("advancedWorkout")) {
+                        workout = user.getJSONObject("advancedWorkout");
+                        workoutDuration = workout.getString("duration");
+                        workoutId = workout.getString("id");
+                        setAdvancedWorkout();
+                    }
                     br.close();
-                }else{
+                } else {
                     System.out.println("COULD NOT FIND USER");
                     return null;
                 }
 
                 conn.disconnect();
-                if(userID == -1) {
+                if (userID == -1) {
                     System.out.println("USER NOT EXISTS");
                     return null;
                 }
-                conn.disconnect();
-
-                ArrayList<Integer> ingredientIds = new ArrayList<>();
-                url = new URL(StaticStrings.ipserver  + "/client/find/" + activity.userId);
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("Accept", "application/json");
-                if(conn.getResponseCode() == 200){
-                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    String output = br.readLine();
-                    JSONArray arr = new JSONArray(output);
-                    for(int i = 0; i<arr.length(); i++){
-                        String ingredientName = arr.getJSONObject(i).getString("name");
-                    }
-                    br.close();
-                    System.out.println("INGREDIENTS: " + arr);
-                }else{
-                    System.out.println("COULD NOT FIND INGREDIENTS");
-                    return null;
-                }
-                conn.disconnect();
-
-            } catch (Exception e) {
-                System.out.println("User could not be created: ");
+            } catch(Exception e){
                 e.printStackTrace();
             }
             return null;
@@ -179,6 +194,129 @@ public class ProgressTrackerFragment extends Fragment {
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
+        }
+
+        protected void setAdvancedWorkout(){
+            try {
+                String currentDailyId = "";
+                JSONObject currentDaily;
+                JSONArray currentDailyExercises = new JSONArray();
+                url = new URL(StaticStrings.ipserver + "/dailyadvancedworkout/findByAdvancedWorkoutId/" + workoutId);
+                conn = (HttpURLConnection) url.openConnection();
+
+                if (conn.getResponseCode() == 200) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String output = br.readLine();
+                    JSONArray arr = new JSONArray(output);
+                    Calendar c = Calendar.getInstance();
+                    String dayName = c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+                    int dayOfWeek = 0;
+                    if (dayName.equals("Monday")) {
+                        dayOfWeek = 1;
+                    } else if (dayName.equals("Tuesday")) {
+                        dayOfWeek = 2;
+                    } else if (dayName.equals("Wednesday")) {
+                        dayOfWeek = 3;
+                    } else if (dayName.equals("Thursday")) {
+                        dayOfWeek = 4;
+                    } else if (dayName.equals("Friday")) {
+                        dayOfWeek = 5;
+                    } else if (dayName.equals("Saturday")) {
+                        dayOfWeek = 6;
+                    } else if (dayName.equals("Sunday")) {
+                        dayOfWeek = 7;
+                    }
+                    for (int i = 0; i < arr.length(); i++) {
+                        Integer weekDay = arr.getJSONObject(i).getInt("week_day");
+                        if (dayOfWeek == weekDay) {
+                            //currentDailyId = arr.getJSONObject(i).getString("id");
+                            currentDailyExercises = arr.getJSONObject(i).getJSONArray("advancedExercises");
+                        }
+                    }
+                    if(null!=currentDailyExercises && currentDailyExercises.length()>0) {
+
+                        for (int i = 0; i < currentDailyExercises.length(); i++) {
+                            exercicesNames.add(currentDailyExercises.getJSONObject(i).getString("exerciseName"));
+                        }
+                    }
+                    getActivity().runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            setSpinnerData(rootView);
+                        }
+                    });
+
+                    br.close();
+                } else {
+                    System.out.println("COULD NOT FIND THE Advanced_Workout");
+                }
+                conn.disconnect();
+            }catch(Exception e){
+                System.out.println("ERROR: Something went wrong");
+                e.printStackTrace();
+            }
+        }
+
+        protected void setBasicWorkout(){
+            try {
+                String currentDailyId = "";
+                JSONObject currentDaily;
+                JSONArray currentDailyExercises = new JSONArray();
+                url = new URL(StaticStrings.ipserver + "/dailybasicworkout/findByBasicWorkoutId/" + workoutId);
+                conn = (HttpURLConnection) url.openConnection();
+
+                if (conn.getResponseCode() == 200) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String output = br.readLine();
+                    JSONArray arr = new JSONArray(output);
+                    Calendar c = Calendar.getInstance();
+                    String dayName = c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+                    int dayOfWeek = 0;
+                    if (dayName.equals("Monday")) {
+                        dayOfWeek = 1;
+                    } else if (dayName.equals("Tuesday")) {
+                        dayOfWeek = 2;
+                    } else if (dayName.equals("Wednesday")) {
+                        dayOfWeek = 3;
+                    } else if (dayName.equals("Thursday")) {
+                        dayOfWeek = 4;
+                    } else if (dayName.equals("Friday")) {
+                        dayOfWeek = 5;
+                    } else if (dayName.equals("Saturday")) {
+                        dayOfWeek = 6;
+                    } else if (dayName.equals("Sunday")) {
+                        dayOfWeek = 7;
+                    }
+                    for (int i = 0; i < arr.length(); i++) {
+                        Integer weekDay = arr.getJSONObject(i).getInt("week_day");
+                        if (dayOfWeek == weekDay) {
+                            currentDailyExercises = arr.getJSONObject(i).getJSONArray("basicExercises");
+                        }
+                    }
+                    if(null!=currentDailyExercises && currentDailyExercises.length()>0) {
+
+                        for (int i = 0; i < currentDailyExercises.length(); i++) {
+                            exercicesNames.add(currentDailyExercises.getJSONObject(i).getString("exerciseName"));
+                        }
+                    }
+
+                    getActivity().runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            setSpinnerData(rootView);
+                        }
+                    });
+                    br.close();
+                } else {
+                    System.out.println("COULD NOT FIND THE Basic_Workout");
+                }
+                conn.disconnect();
+            }catch(Exception e){
+                System.out.println("ERROR: Something went wrong");
+                e.printStackTrace();
+            }
         }
     }
 }
